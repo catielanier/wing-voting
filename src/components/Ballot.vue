67bf5da54ffb943b603c4406c8849a98d52ac4ea
Catie Lanier <hello@catielanier.ca>
2025-11-15 22:02:32 -0500

    Adds ballot component with drag and drop
    Implements a ballot component that allows users to rank
    options by dragging and dropping them into their desired order.
    
    Also, renames the application title.

--- src/components/Ballot.vue
+++ src/components/Ballot.vue
@@ -1,11 +1,36 @@
 <script lang="ts">
-  export default {
-    name: 'Ballot'
-  }
+  import { defineComponent, ref } from 'vue';
+  import draggable from 'vuedraggable';
+  import { wings } from '../data/choices';
+
+  export default defineComponent({
+    name: 'Ballot',
+    components: { draggable },
+    setup() {
+      const ballot = ref<string[]>([...wings]);
+      return { ballot };
+    }
+  });
 </script>
 
 <template>
-  <div>Ballot Component</div>
+  <div>
+    <h1>Vote for your favourite Whiff City sauce flavour!</h1>
+    <draggable
+        v-model="ballot"
+        :list="ballot"
+        class="list-group"
+        ghost-class="ghost"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in ballot"
+          :key="element"
+        >
+          {{ element }}
+        </div>
+      </draggable>
+  </div>
 </template>
 
 <style>
