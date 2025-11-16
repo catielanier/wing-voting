434fc9065e8e8a14cfef198d2adc94ad926e2470
Catie Lanier <hello@catielanier.ca>
2025-11-15 19:41:07 -0500

    add ballot choices

---
+++ src/data/choices.ts
@@ -0,0 +1,10 @@
+export const wings: string[] = [
+  "Fei-Long",
+  "Whiff City Wings",
+  "Easy Win(g)s",
+  "Big Dill Energy",
+  "Toe-tally Canadian Jerk",
+  "Tiger's Bite",
+  "Wing Chun",
+  "Touch Grass"
+];
